315ce28ace888851bfa6eea4e3645a39f4b75a77
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-11-04 10:01:51 +0700

    mantap httprouter

---
+++ method_not_allowed_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestMethodNotAllowed(t *testing.T) {
+
+	router := httprouter.New()
+	router.MethodNotAllowed = http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Salah Method Bjir")
+	})
+	router.POST("/", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		fmt.Fprint(writer, "POST")
+	})
+
+	request := httptest.NewRequest("PUT", "http://localhost:3000/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Salah Method Bjir", string(body))
+
+}
